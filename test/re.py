f87a80a0640ed537eb3636a80bedfa418e57ac16
Sun <s-c@outlook.com>
2015-09-28 14:22:26 +0800

    f

--- test/re.py
+++ test/re.py
@@ -3,3 +3,7 @@
 import re
 print(re.match('www','www.runoob.com').span())
 print(re.match('com','www.runoob.com'))
+
+line='Cats are smarter than dogs'
+matchObj=re.match(r'(.*) are (.*?) .*',line,re.M|re.I)
+
